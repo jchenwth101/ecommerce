7db6795449e8a4a3a2af1d12a4a97c6dbf691fbf
Joel Chenoweth <jchenwth@gmail.com>
2022-04-23 04:49:20 -0500

    updated settings.py

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '56fi&j$1yd33cf$1%tupzhlngah17l(*)n^h8p*oh6icv93(h='
+SECRET_KEY = os.environ.get('BLOG_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
